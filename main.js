8eef1179d7c74f9fe995edb8400b8946aed3460b
Aribzhan <aribzankamilzanov@gmail.com>
2026-03-17 14:33:34 +0500

    new fix

--- main.js
+++ main.js
@@ -44,7 +44,7 @@ app.use((err, req, res, next) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 7534;
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port http://localhost:${PORT}`);
